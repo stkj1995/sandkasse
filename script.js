8e885414da137cee05176c231434de3527026abb
Sophie <soph1155@stud.kea.dk>
2024-01-31 22:19:13 +0100

    øvelse 2

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*test1*/
+
 function checkAlcohol(element) {
   if (element.alt === "beer" || element.alt === "snaps") {
     console.log("Indeholder alkohol");
@@ -5,3 +7,23 @@ function checkAlcohol(element) {
     console.log("Alkoholfri");
   }
 }
+
+/*test2*/
+
+function getTimeOfDay() {
+  const currentHour = new Date().getHours();
+
+  if (currentHour >= 5 && currentHour < 10) {
+    console.log("Godmorgen");
+  } else if (currentHour >= 10 && currentHour < 18) {
+    console.log("Goddag");
+  } else if (currentHour >= 18 && currentHour < 24) {
+    console.log("Godaften");
+  } else {
+    console.log("Godnat");
+  }
+}
+
+getTimeOfDay();
+
+
