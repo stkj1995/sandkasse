7abe0fac4e7a9014d5e93fe509a0b6ba1b8db978
Sophie <soph1155@stud.kea.dk>
2024-01-31 22:14:30 +0100

    drikkevarer test

---
+++ script.js
@@ -0,0 +1,7 @@
+function checkAlcohol(element) {
+  if (element.alt === "beer" || element.alt === "snaps") {
+    console.log("Indeholder alkohol");
+  } else {
+    console.log("Alkoholfri");
+  }
+}
